081219a339adf6bc415ba5958f750182d141d084
Angel D. Munoz <scythes.lol@gmail.com>
2022-07-07 12:14:11 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+[bun.sh]: https://bun.sh
+
+# [Bun.sh] + Fable Playground
+
+This is a small and experimental repo to add a few bindings for the node.js new competitor runtime [bun.sh]
+
+Although this is not something serious yet, if you actually want to work with me to make this a true bindings project let me know!
+I'd be glad to work towards something like that
+
+# Requirements
+
+- .NET6 and above - https://get.dot.net
+- Bun - [bun.sh]
---
+++ src/Bun.fs
@@ -0,0 +1,96 @@
+[<AutoOpen>]
+module Bun
+
+open Fable.Core
+open Fable.Core.JsInterop
+open Browser.Types
+open Fetch
+
+type BlobPart = U4<string, Blob, JS.ArrayBuffer, JS.ArrayBufferView>
+type StringOrBuffer = U3<string, JS.TypedArray, JS.ArrayBuffer>
+
+type Response with
+    [<Emit("new Response($0)")>]
+    static member create(content: U2<BlobPart, BlobPart array>) = jsNative
+
+    [<Emit("new Response($0, $1)")>]
+    static member create(content: U2<BlobPart, BlobPart array>, resInit: obj) = jsNative
+
+type MMapOptions =
+    abstract sync: bool option
+    abstract shared: bool option
+
+[<Erase; StringEnum>]
+type Editors =
+    | [<CompiledName "vscode">] VSCode
+    | [<CompiledName "subl">] Submile
+
+[<Erase; StringEnum>]
+type DigestEncoding =
+    | [<CompiledName "hex">] Hex
+    | [<CompiledName "base64">] Base64
+
+type EditorOptions =
+    abstract member editor: Editors option
+    abstract line: float option
+    abstract column: float option
+
+type BunServeOptions =
+    abstract port: int option
+    abstract fetch: Request -> U2<Response, JS.Promise<Response>>
+    abstract error: exn -> U2<Response, JS.Promise<Response>>
+
+type BunServer =
+    abstract stop: unit -> unit
+    abstract pendingRequests: float
+    abstract hostname: string
+    abstract development: bool
+
+
+type PathLike = U3<string, JS.TypedArray, JS.ArrayBuffer>
+
+[<Erase>]
+type unsafe =
+    [<Emit("$0.arrayBufferToString($1)")>]
+    member _.arrayBufferToString(buffer: U2<JS.Uint8Array, JS.ArrayBuffer>) : string = jsNative
+
+    [<Emit("$0.arrayBufferToString($1)")>]
+    member _.arrayBufferToString(buffer: JS.Uint16Array) : string = jsNative
+
+    [<Emit("$0.segfault()")>]
+    member _.segfault: unit = jsNative
+
+[<Global; Erase>]
+type Bun =
+    static member serve(options: obj) : BunServer = jsNative
+    static member resolveSync(moduleId: string, parent: string) : string = jsNative
+    static member resolve(moduleId: string, parent: string) : JS.Promise<string> = jsNative
+
+    static member write
+        (
+            destination: U2<Blob, PathLike>,
+            input: U4<Blob, JS.TypedArray, string, BlobPart[]>
+        ) : JS.Promise<float> =
+        jsNative
+
+    static member write(destinationPath: PathLike, input: Response) : JS.Promise<float> = jsNative
+    static member write(destination: Blob, input: Blob) : JS.Promise<float> = jsNative
+    static member write(destinationPath: PathLike, input: Blob) : JS.Promise<float> = jsNative
+    static member file(path: string, ?options: BlobPropertyBag) : Blob = jsNative
+    static member file(path: U2<JS.ArrayBuffer, JS.Uint8Array>, ?options: BlobPropertyBag) : Blob = jsNative
+    static member file(fileDescriptor: float, ?options: BlobPropertyBag) : Blob = jsNative
+    static member allocUnsafe(size: float) : JS.Uint8Array = jsNative
+    static member inspect([<System.ParamArray>] values: obj[]) : string = jsNative
+    static member mmap(path: PathLike, ?options: MMapOptions) : JS.Uint8Array = jsNative
+    static member stdout: Blob = jsNative
+    static member stderr: Blob = jsNative
+    static member stdin: Blob = jsNative
+    static member unsafe: unsafe = jsNative
+    static member enableANSIColors: bool = jsNative
+    static member main: string = jsNative
+    static member gc(foce: bool) : unit = jsNative
+    static member nanoseconds() : int64 = jsNative
+    static member shrink() : unit = jsNative
+    static member openInEditor(path: string, ?options: EditorOptions) : unit = jsNative
+    static member sha(input: StringOrBuffer, ?hashInto: JS.Uint8Array) : JS.Uint8Array = jsNative
+    static member sha(input: StringOrBuffer, encoding: DigestEncoding) : string = jsNative
