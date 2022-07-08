6a93aa9e51dab6dad8210f98fe06145022771b65
Angel D. Munoz <scythes.lol@gmail.com>
2022-07-08 00:35:01 -0500

    add extensions

--- src/Fable.Bun.Sample/Extensions.fs
+++ src/Fable.Bun.Sample/Extensions.fs
@@ -2,6 +2,19 @@
 module Extensions
 
 open Fable.Core
+open Fetch
+open Browser.Types
+open Bix.Types
 
 [<Emit("{...$0, ...$1}")>]
 let inline mergeObjects (o1, o2) = jsNative
+
+[<Emit("new Response($0, $1)")>]
+let inline private createResponseInit (content: obj) (options: obj) = jsNative
+
+let compose (h1: HttpHandler) (h2: HttpHandler) : HttpHandler =
+    fun final ->
+        let fn = final |> h1 |> h2
+        fun ctx -> fn ctx
+
+let inline (>=>) h1 h2 = compose h1 h2
