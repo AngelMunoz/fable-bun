3592f9d64941935f7b190ca827641bb45514d404
Angel D. Munoz <scythes.lol@gmail.com>
2022-07-08 00:34:46 -0500

    add bix handler

--- src/Fable.Bun.Sample/Bix.fs
+++ src/Fable.Bun.Sample/Bix.fs
@@ -1,16 +1,20 @@
 [<RequireQualifiedAccess>]
-module Bix
+module Bix.Server
 
 open Fable.Core
 open Fable.Core.JsInterop
+open Browser.Types
 open Fetch
-open Types
-open Bun
+open Bix.Types
+open Fable.Bun
 
 
-let Server = ResizeArray<BixServerArgs>()
+[<Emit "new URL($0)">]
+let inline private createUrl (url: string) : URL = jsNative
 
-let inline withOptions (args: seq<BixServerArgs>) = ResizeArray<BixServerArgs>(args)
+let Bix = ResizeArray<BixServerArgs>()
+
+let inline BixArgs (args: seq<BixServerArgs>) = ResizeArray<BixServerArgs>(args)
 
 let inline withPort (port: int) (args: ResizeArray<BixServerArgs>) =
     args.Add(Port port)
@@ -95,3 +99,46 @@ let inline run (args: seq<BixServerArgs>) =
         let options = mergeObjects (restArgs, names)
         Bun.serve (unbox options)
     | None -> Bun.serve (unbox restArgs)
+
+let BixHandler (routes: RouteMap) (req: Request) : JS.Promise<Response> =
+    let ctx = HttpContext(JsInterop.jsThis, req)
+
+    promise {
+        let url = createUrl req.url
+        let verb = RouteType.FromString req.method
+        let handler = routes |> Map.tryFind (verb, url.pathname)
+
+        let! res =
+            promise {
+                match handler with
+                | Some handler -> return! handler (fun _ -> promise { return None }) ctx
+                | None -> return! Promise.lift None
+            }
+
+        match res with
+        | None -> return Response.create ("", [])
+        | Some result ->
+            match result with
+            | Text value ->
+                return Response.create (value, [ ResponseInitArgs.Headers [| "content-type", "text/plain" |] ])
+            | Html value ->
+                return Response.create (value, [ ResponseInitArgs.Headers [| "content-type", "text/html" |] ])
+            | Json value ->
+                let content = JS.JSON.stringify (value)
+                return Response.create (content, [ ResponseInitArgs.Headers [| "content-type", "application/json" |] ])
+            | Blob (content, mimeType) ->
+                return Response.create (content, [ ResponseInitArgs.Headers [| "content-type", mimeType |] ])
+            | ArrayBufferView (content, mimeType) ->
+                return Response.create (content, [ ResponseInitArgs.Headers [| "content-type", mimeType |] ])
+            | ArrayBuffer (content, mimeType) ->
+                return Response.create (content, [ ResponseInitArgs.Headers [| "content-type", mimeType |] ])
+            | BixResponse.Custom (content, args) ->
+                // it might not be a string but we don't really care
+                return Response.create (unbox<string> content, args)
+    }
+
+
+
+let inline withBixHandler (routes: RouteMap) (args: ResizeArray<BixServerArgs>) =
+    args.Add(Fetch(unbox (BixHandler routes)))
+    args
